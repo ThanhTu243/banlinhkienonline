1c05054514af703daa9648a4aaf9731fa46f5923
nthanhtu243 <nthanhtu243@gmail.com>
2022-07-02 19:09:05 +0700

    fix image for cart

--- src/main/java/com/thanhtu/crud/model/dto/fk/CartFKViewDto.java
+++ src/main/java/com/thanhtu/crud/model/dto/fk/CartFKViewDto.java
@@ -1,13 +1,16 @@
 package com.thanhtu.crud.model.dto.fk;
 
+import com.thanhtu.crud.model.dto.ProductImageDto;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class CartFKViewDto {
     private Integer cartId;
     private Integer productId;
     private String nameProduct;
-    private String productImage;
+    private List<ProductImageDto> productImageDtoSet;
     private Integer quantity;
     private Integer discount;
     private Integer unitPrice;
--- src/main/java/com/thanhtu/crud/model/mapper/CartMapper.java
+++ src/main/java/com/thanhtu/crud/model/mapper/CartMapper.java
@@ -3,8 +3,12 @@ package com.thanhtu.crud.model.mapper;
 import com.thanhtu.crud.entity.*;
 import com.thanhtu.crud.model.dto.CartByCustomerDto;
 import com.thanhtu.crud.model.dto.CartDto;
+import com.thanhtu.crud.model.dto.ProductImageDto;
 import com.thanhtu.crud.model.dto.fk.CartFKViewDto;
 import com.thanhtu.crud.model.request.CartRequest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CartMapper {
     public static CartEntity toCartEntity(CartRequest cartRequest, ProductEntity productEntity, CustomerEntity customerEntity)
@@ -35,8 +39,16 @@ public class CartMapper {
         tmp.setDiscount(product.getDiscount());
         tmp.setPriceAfterDiscount(Long.valueOf(100-product.getDiscount())*Long.valueOf(product.getUnitPrice())/100);
         tmp.setQuantity(cart.getQuantity());
-
         tmp.setCost(Long.valueOf(tmp.getPriceAfterDiscount()* cart.getQuantity()));
+        List<ProductImageDto> productImageDtoList=new ArrayList<>();
+        for(ProductImage productImage:product.getProductImageSet())
+        {
+            ProductImageDto productImageDto=new ProductImageDto();
+            productImageDto.setImageId(productImage.getImageEntity().getImageId());
+            productImageDto.setImage(productImage.getImageEntity().getImage());
+            productImageDtoList.add(productImageDto);
+        }
+        tmp.setProductImageDtoSet(productImageDtoList);
         return tmp;
     }
 }
